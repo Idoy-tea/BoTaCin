a3e492df4654a3a4d58ed768adf8cda5a8b9fec7
Idoy-tea <idoyrqdank@gmail.com>
2024-01-15 14:28:26 +0700

    Create maker.js

---
+++ coin/maker.js
@@ -0,0 +1,24 @@
+const { getDetachSourceFromOHLCV, sma, rsiCheck } = require('trading-indicator');
+
+class Mkr {
+  constructor(timeFrame) {
+    this.timeFrame = timeFrame;
+  };
+
+  async getOHLCVData() {
+    return getDetachSourceFromOHLCV('binance', 'MKR/USDT', this.timeFrame, false);
+  };
+
+  async getSma(period) {
+    const { input } = await this.getOHLCVData();
+    let smaData = await sma(period, "close", input);
+    return smaData//[smaData.length - 1];
+  };
+
+  async getRsiCross() {
+    const { input } = await this.getOHLCVData();
+    return rsiCheck(14, 70, 30, input);
+  };
+};
+
+module.exports = Mkr;
